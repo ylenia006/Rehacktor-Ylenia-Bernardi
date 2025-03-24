6776338e3c5d6262661bc62e7e4124e1e439f911
YleniaBernardi <y.bernardi@comverta.com>
2025-03-24 17:51:20 +0100

    abbiamo visto che il build in locale sembra funzionare

--- src/pages/Genre/index.jsx
+++ src/pages/Genre/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import style from '../../Home/Home.module.css';
+import style from '../Home/Home.module.css';
 import GameCard from "../../components/GameCard";
 import Sidebar from "../../components/Sidebar";
 
