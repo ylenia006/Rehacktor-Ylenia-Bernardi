3088630d49c7a7c7d3685ef9e18a1a19102e161d
YleniaBernardi <y.bernardi@comverta.com>
2025-03-24 17:10:49 +0100

    rimosso doppio import sidebar in index.jsx

--- src/pages/Genre/index.jsx
+++ src/pages/Genre/index.jsx
@@ -3,7 +3,6 @@ import { useParams } from 'react-router';
 import style from '@/Home/Home.module.css';
 import GameCard from "@/components/GameCard";
 import Sidebar from "@/components/Sidebar";
-import Sidebar from "@/components/Sidebar";
 
 export default function Genre() {
 
